c5615ad8882a7bea7599f58d926916fa9847b46a
AxelBai8 <axxelb14@gmail.com>
2025-08-11 15:39:31 -0600

    added the new file for soraflight

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       function clearNameIntervals() {
         nameAnimationIntervals.forEach(interval => clearInterval(interval));
+        
         nameAnimationIntervals = [];
       }
 
